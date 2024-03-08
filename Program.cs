26250e0a1eb81c5dd623305fde9f83505d86f5ca
Siphesihle <madondosphesihles@gmail.com>
2024-03-08 14:41:50 +0200

    Reapply "Add project files."
    This reverts commit 2fe2705b5eee935e895f1e2e389dd707c31d739b.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Homes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HomesContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("allowangularorigins",
+    builder =>
+    {
+        builder.WithOrigins(
+                            "http://localhost:4200"
+                            )
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+app.UseCors("allowangularorigins");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
